77ed94f96de9c16f224128ce633580e099bd0e29
Özge Poyraz <ozge.poyraz@softtech.com.tr>
2026-06-12 13:15:48 +0300

    fix: remove unused @_exported imports from static build

--- Sources/PlateauMobile/PlateauMobile.swift
+++ Sources/PlateauMobile/PlateauMobile.swift
@@ -3,15 +3,7 @@
 
 import Foundation
 
-// Re-export bağımlılıklar
 @_exported import PlateauMobile
-@_exported import Starscream
-@_exported import Lottie
-@_exported import DGCharts
-@_exported import Mantis
-@_exported import AppAuth
-@_exported import SDWebImage
-@_exported import SDWebImageSVGCoder
 
 public struct PlateauMobileVersion {
     public static let version = "1.5.3"
